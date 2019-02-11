567399451dbba6d51b46cf857c641a4ad4ec35c1
David <David@David-PC>
2019-02-11 21:31:50 +0100

    Borrar

--- public_html/formulario.js
+++ public_html/formulario.js
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 var numForm = 0;
+var numInput = 0;
 
-var nombreForm=document.getElementById("nombreForm");
+var nombreForm = document.getElementById("nombreForm");
 var contenido = document.getElementById("contenido");
 
 
@@ -15,29 +16,36 @@ function genera_formulario() {
     formulario.id = nombreForm.value;
     formulario.method = 'POST';
 
-    my_tb = document.createElement('INPUT');
-    my_tb.type = 'TEXT';
-    my_tb.name = 'myInput';
-    formulario.appendChild(my_tb);
-
     formulario.setAttribute("border", "1");
     formulario.style.width = "400px";
     formulario.style.height = "400px";
     formulario.style.background = "red";
 
 
-    formulario.appendChild(my_tb);
-    
     //document.body.appendChild(formulario);
     contenido.appendChild(formulario);
     numForm++;
+
+
 }
 
 function borrar_formulario() {
-    
+
     //alert(document.getElementById("nombreForm").value);
     //document.getElementsById(document.getElementById("nombreForm").value).remove();
     var item = document.getElementById(document.getElementById("nombreForm").value);
     contenido.removeChild(item);
+}
+
+function anadir_input() {
+
+    var my_tb = document.createElement('INPUT');
+    my_tb.type = 'TEXT';
+    my_tb.name = 'myInput';
+    my_tb.id=numInput;
+    numInput++;
+
+    var item = document.getElementById(document.getElementById("nombreForm").value);
+    item.appendChild(my_tb);
 }
    
