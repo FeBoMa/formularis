bf53cf2f38710bedbb0af3b2c9baa05fb1ac7f66
David <David@David-PC>
2019-02-20 19:32:04 +0100

    CheckBox acabado y funcionando

--- public_html/formulario.js
+++ public_html/formulario.js
@@ -23,6 +23,9 @@ function genera_formulario() {
     var divRadioButton = document.createElement('div');
     divRadioButton.id = 'divRadioButton';
 
+    var divRadioCH = document.createElement('div');
+    divRadioCH.id = 'divRadioCH';
+
     //INPUTS---------------------------------------------------------------
 
     var tabDivInputs = document.createElement("TABLE");
@@ -136,10 +139,10 @@ function genera_formulario() {
     }
 
     //RADIO BUTTON-------------------------------------------------------------
+   
     var tabDivRB = document.createElement("TABLE");
     tabDivRB.setAttribute("id", "tabDivRB");
     divRadioButton.appendChild(tabDivRB);
-
 
     var trTabDivRB = document.createElement("TR");
     trTabDivRB.setAttribute("id", "trTabDivRB");
@@ -221,19 +224,116 @@ function genera_formulario() {
         }
     }
 
-//
-    divInputs.appendChild(tabDivInputs);
-    formulario.appendChild(divInputs);
+//CHECK BOX--------------------------------------------------------------------
+
+    var tabDivCH = document.createElement("TABLE");
+    tabDivCH.setAttribute("id", "tabDivCH");
+    divRadioCH.appendChild(tabDivCH);
+
+    var trTabDivCH = document.createElement("TR");
+    trTabDivCH.setAttribute("id", "trTabDivCH");
+    tabDivCH.appendChild(trTabDivCH);
+
+    var txtCheck = document.createElement('INPUT');
+    txtCheck.type = 'text';
+    txtCheck.id = 'txtRadio';
+
+    var tdTabtxtCH = document.createElement("TD");
+    tdTabtxtCH.appendChild(txtCheck);
+    trTabDivCH.appendChild(tdTabtxtCH);
+
+    var btnCheck = document.createElement('INPUT');
+    btnCheck.type = 'button';
+    btnCheck.name = 'añadirCheck';
+    btnCheck.id = 'inputCheck';
+    btnCheck.value = "Añadir CheckBox";
+
+    var tdCheck = document.createElement("TD");
+    tdCheck.appendChild(btnCheck);
+    trTabDivCH.appendChild(btnCheck);
+
+    //ONCLICK CHECKBOX
+
+    btnCheck.onclick = function () {
+
+        var trTabDivCH = document.createElement("TR");
+        trTabDivCH.setAttribute("id", "trInput");
+        tabDivCH.appendChild(trTabDivCH);
+
+        var checkCreate = document.createElement('INPUT');
+        checkCreate.type = 'checkbox';
+        checkCreate.id = 'checkbox';
+        checkCreate.value = txtCheck.value;
+        checkCreate.textContent = txtCheck.value;
+        checkCreate.name = 'grupo';
+
+        var label = document.createElement('LABEL');
+        label.textContent = txtCheck.value;
+
+        var tdCheck = document.createElement("TD");
+        tdCheck.id = 'idCheck';
+        tdCheck.appendChild(label);
+        trTabDivCH.appendChild(tdCheck);
+
+        tdCheck.insertBefore(checkCreate, label);
+
+        var btnEdit = document.createElement('INPUT');
+        btnEdit.type = 'button';
+        btnEdit.name = 'edit';
+        btnEdit.id = 'edit';
+        btnEdit.value = "Edit";
+
+        var tdTabBtnEdit = document.createElement("TD");
+        tdTabBtnEdit.appendChild(btnEdit);
+        trTabDivCH.appendChild(tdTabBtnEdit);
+
+
+        btnEdit.onclick = function () {
+            checkCreate.value = txtCheck.value;
+            label.textContent = txtCheck.value;
+        }
+
+        var btnDel = document.createElement('INPUT');
+        btnDel.type = 'button';
+        btnDel.name = 'del';
+        btnDel.id = 'del';
+        btnDel.value = "Eliminar";
+
+        var tdTabBtnDel = document.createElement("TD");
+        tdTabBtnDel.appendChild(btnDel);
+        trTabDivCH.appendChild(tdTabBtnDel);
+
+
+        btnDel.onclick = function () {
+            tabDivCH.removeChild(trTabDivCH);
+        }
+    }
+
+
+
+
+
+
+    //Añadimos el formulario al arbol como hijo de contenido
     contenido.appendChild(formulario);
-    divRadioButton.appendChild(tabDivRB);
+    //Añadimos el div y la tabla(tabDivInputs) de los inputs al formulario
+    formulario.appendChild(divInputs);
+    divInputs.appendChild(tabDivInputs);
+    //Añadimos el div y la tabla(tabDivInputs) de los radiobutton al formulario
     formulario.appendChild(divRadioButton);
+    divRadioButton.appendChild(tabDivRB);
+    //Añadimos el div y la tabla(tabDivInputs) de los checkbox al formulario
+    formulario.appendChild(divRadioCH);
+    divRadioCH.appendChild(tabDivCH);
+
+
+
+    function borrar_formulario() {
+
+        //alert(document.getElementById("nombreForm").value);
+        //document.getElementsById(document.getElementById("nombreForm").value).remove();
+        var item = document.getElementById(document.getElementById("nombreForm").value);
+        contenido.removeChild(item);
+    }
+
 }
-
-function borrar_formulario() {
-
-    //alert(document.getElementById("nombreForm").value);
-    //document.getElementsById(document.getElementById("nombreForm").value).remove();
-    var item = document.getElementById(document.getElementById("nombreForm").value);
-    contenido.removeChild(item);
-}
-
